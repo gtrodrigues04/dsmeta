0bd45a5edf77fa0068d62951eebad3588cdac729
Guilherme Teixeira <grodrigues813@gmail.com>
2022-09-09 14:31:16 -0300

    Security Config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import './styles.css'
-import logo from '../../assets/img/logo.svg'
+import './styles.css';
+import logo from '../../assets/img/logo.svg';
 
 export default function Header() {
   return (
