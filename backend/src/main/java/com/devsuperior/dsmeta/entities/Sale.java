501a0c125be892d03ecde2a41743bff6d9c66faf
Guilherme Teixeira <grodrigues813@gmail.com>
2022-09-09 15:16:37 -0300

    Rest api

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -10,11 +10,13 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="tb_sales")
 public class Sale {
